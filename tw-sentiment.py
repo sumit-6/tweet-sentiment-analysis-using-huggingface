6d0a287ac396fd73f01bd76509af7ac4e13326bc
Sumit Verma <75926522+sumit-6@users.noreply.github.com>
2023-11-29 22:32:59 +0530

    Add files via upload

---
+++ tw-sentiment.py
@@ -0,0 +1,33 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from scipy.special import softmax
+
+tweet = "@Shubham today's cold @ home 🥶 https://www.instagram.com/shubham_goell/?hl=en"
+
+# preprocess tweet
+tweet_words = []
+for word in tweet.split(' '):
+    if word.startswith('@') and len(word) > 1:
+        word = '@user'
+    elif word.startswith('http'):
+        word = 'http'
+    tweet_words.append(word)
+
+tweet_proc = ' '.join(tweet_words)
+
+# load model and tokenizer
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+model = AutoModelForSequenceClassification.from_pretrained(roberta)
+tokenizer = AutoTokenizer.from_pretrained(roberta)
+
+labels = ["Negative", "Neutral", "Positive"]
+
+# sentiment analysis
+encoded_tweet = tokenizer(tweet_proc, return_tensors = 'pt')
+
+output = model(encoded_tweet['input_ids'], encoded_tweet['attention_mask'])
+scores = output[0][0].detach().numpy()
+scores = softmax(scores)
+for i in range(len(scores)):
+    l = labels[i]
+    s = scores[i]
+    print(l, s)
